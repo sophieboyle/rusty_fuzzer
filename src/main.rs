f5650399d46ba94c7071881811d43501aa33848f
Sophie Boyle <sophieboyle4028@gmail.com>
2022-03-02 01:38:12 +0000

    Renamed project and added reading bytes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::fs::read;
+
+fn get_bytes(filename : String) -> Vec<u8>{
+    let bytes_vector = read(filename).unwrap();
+    return bytes_vector;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: cargo run <jpg-path>");
+    }
+
+    let bytes = get_bytes(args[1].clone());
+    print!("Test");
 }
